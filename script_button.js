eb65426b5acdbbe5eda642bf38465323193139a6
VladimirTagarov <tagarovl@mail.ru>
2023-04-09 14:35:30 +0700

    added script_button, added block in index.html

---
+++ script_button.js
@@ -0,0 +1,46 @@
+function gameForButton() {
+    let monthNumber = prompt('Введите номер месяца в году');
+switch (monthNumber) {
+    case '1':
+        alert('Зима')
+        break;
+    case '2':
+        alert('Зима')
+        break;
+    case '3':
+        alert('Весна')
+        break;
+    case '4':
+        alert('Весна')
+        break;
+    case '5':
+        alert('Весна')
+        break;
+    case '6':
+        alert('Лето')
+        break;
+    case '7':
+        alert('Лето')
+        break;
+    case '8':
+        alert('Лето')
+        break;
+    case '9':
+        alert('Осень')
+        break;
+    case '10':
+        alert('Осень')
+        break;
+    case '11':
+        alert('Осень')
+        break;
+    case '12':
+        alert('Зима')
+        break;
+
+    default:
+        alert('Такого номера месяца вовсе не существует')
+        break;
+}
+}
+
